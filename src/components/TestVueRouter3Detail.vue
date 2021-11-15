46ea09bdc042292e4dee8eab5295c63b6affec32
woow_wu7 <woow.wu7@gmail.com>
2021-11-15 10:58:03 +0800

    feat: vue-router2.

--- src/components/TestVueRouter3Detail.vue
+++ src/components/TestVueRouter3Detail.vue
@@ -4,6 +4,7 @@
 </template>
 
 <script>
+import { onBeforeRouteUpdate } from 'vue-router';
 export default {
   data() {
     return {
@@ -25,6 +26,12 @@ export default {
   // 解释：
   // - 为什么？：这里之所以不使用 data.name + 在watch中$route中在去修改this.name
   // - 因为：使用计算属性可以使得：数据的修改逻辑更清晰，并且高内聚
+  updated() {
+    console.log('当使用路由参数时，组件将被复用，同时也说明组件的生命周期不在被复用')
+  },
+  beforeRouteUpdate() {
+    console.log('组件')
+  }
 };
 </script>
 
